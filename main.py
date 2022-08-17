b11b7edca94efc2ab6e9d1e721a3f5d39941b3a4
danielvictor <automacao@caferancheiro.com.br>
2022-08-17 09:46:45 -0300

    tird

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import datetime
-import os
 import time
-
+import sys
 
 
 
@@ -11,11 +10,13 @@ bot = TelegramBot('5709111053:AAGnHL5ZJei7kAJEA89oM1Y_EyfpuKByfqs')
 HORA_MIN = 8
 HORA_MAX = 20
 print("BOT TELEGRAM ALIVE")
+sys.stdout.flush()
 while(True):
-    time.sleep(60)
+    time.sleep(30)
     bot.getMessages()
     hora = datetime.datetime.now().time().hour
     if(hora > HORA_MIN and hora << HORA_MAX):
         print(hora)
+        sys.stdout.flush()
         bot.sendMessage()
         time.sleep((60*60)*2)
